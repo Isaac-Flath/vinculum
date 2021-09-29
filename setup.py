929eae0ee1425507d0e3a7feac90ad0ad645d964
Isaac Flath <Isaac.flath@gmail.com>
2021-09-28 20:38:31 -0400

    bug fix on pip package build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='vinculum',
-    packages=find_packages(include=['vinculum']),
+    packages=find_packages(exclude=['tests']),
     version='0.1.0',
     description='Various utilities and functions to make Vinculum stuff easier',
     author='Isaac FLath',
